8f15f2c39cd5deeaa514584cf13965973ae523d4
prathap442 <pmohansaikrishna@gmail.com>
2020-07-18 17:54:01 +0530

    struck: when navigating to other page

--- jerht_ca_admin.py
+++ jerht_ca_admin.py
@@ -3,15 +3,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import unittest
 import pdb
+import time
 
+BASE_URL = "http://ec2-34-226-191-16.compute-1.amazonaws.com"
 class JerhtCaAdmin(unittest.TestCase):
+    #setUp methods will be made to run before every test case .
     def setUp(self):
         self.driver = webdriver.Chrome()
-    
-    def test_login_works(self):
         driver = self.driver
         # To go to the sight where we have one session here
-        driver.get('http://ec2-34-226-191-16.compute-1.amazonaws.com/login')
+        driver.get(BASE_URL + "/login")
+        driver.get(BASE_URL+"/login")
         email_handler = driver.find_element_by_name('email')
         email_handler.clear()
         email_handler.send_keys('vinney@yopmail.com')
@@ -20,14 +22,22 @@ class JerhtCaAdmin(unittest.TestCase):
         password_handler.clear()
         password_handler.send_keys('password@123')
         password_handler.send_keys(Keys.RETURN)
-        pdb.set_trace()
-        assert "CERTIFICATES" in driver.page_source
 
-    def test_employee_fileupload(self):
-        
-    
     def tearDown(self):
-        self.driver.close()
+        driver = self.driver
+        #driver.close()    
+    
+    # def test_login_works(self):
+    #     driver = self.driver
+        
+    def test_employee_fileupload(self):
+        driver = self.driver
+        print(driver.current_url)
+        assert driver.page_source.find("CERTIFICATES")
+        driver.get(BASE_URL+"/organization")
+        pdb.set_trace()
+        driver.findElement(By.xpath('//*[@id="root"]/div/div/div[1]/div/div[1]/div[2]/ul/a[2]')).click()
+
 
 
 if __name__ == "__main__":
